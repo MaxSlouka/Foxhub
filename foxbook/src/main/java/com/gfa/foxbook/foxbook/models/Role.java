0ef748f073105c2095728c111c329dda6b29c18c
Max Nesazal <maxnesazal1492@gmail.com>
2023-07-03 15:24:34 +0200

    add skeleton of role model

--- foxbook/src/main/java/com/gfa/foxbook/foxbook/models/Role.java
+++ foxbook/src/main/java/com/gfa/foxbook/foxbook/models/Role.java
@@ -1,4 +1,16 @@
 package com.gfa.foxbook.foxbook.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
 }
