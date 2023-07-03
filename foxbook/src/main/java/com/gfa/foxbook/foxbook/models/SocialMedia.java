889f9f52b8821bd62a3699e626fea0752c60d4e8
Max Nesazal <maxnesazal1492@gmail.com>
2023-07-03 16:30:10 +0200

    add skeleton of SocialMedia

--- foxbook/src/main/java/com/gfa/foxbook/foxbook/models/SocialMedia.java
+++ foxbook/src/main/java/com/gfa/foxbook/foxbook/models/SocialMedia.java
@@ -1,4 +1,16 @@
 package com.gfa.foxbook.foxbook.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class SocialMedia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
 }
