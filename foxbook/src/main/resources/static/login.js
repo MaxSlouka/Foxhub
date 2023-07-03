e428fdad9d62f7186d0afb72d0016e547063b3dc
TomasMacoun <macount@seznam.cz>
2023-07-03 17:18:41 +0200

    Created login.js file

---
+++ foxbook/src/main/resources/static/login.js
@@ -0,0 +1,38 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const newPostForm = document.querySelector("form");
+    const message = document.querySelector(".message");
+    message.textContent = "";
+
+    newPostForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(e.target);
+        const task = Object.fromEntries(formData);
+
+        fetch("/authentication", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(task)
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error("Error occurred!");
+                }
+            })
+            .then(data => {
+                console.log("Provided data: ");
+                console.log(data);
+            })
+            .catch(error => {
+                console.error("Error:", error);
+            });
+
+        newPostForm.reset();
+    });
+});
