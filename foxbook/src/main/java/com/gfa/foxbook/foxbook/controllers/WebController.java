528061a2ab19eba891197671f46781d3f343418e
DeenO37 <deen.bds@gmail.com>
2023-07-03 14:58:21 +0200

    add index getMapping

--- foxbook/src/main/java/com/gfa/foxbook/foxbook/controllers/WebController.java
+++ foxbook/src/main/java/com/gfa/foxbook/foxbook/controllers/WebController.java
@@ -1,7 +1,13 @@
 package com.gfa.foxbook.foxbook.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
+
+ @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
