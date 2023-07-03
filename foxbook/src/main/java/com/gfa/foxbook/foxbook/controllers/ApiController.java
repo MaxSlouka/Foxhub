ce6e583a174ec3b70221ba6535f6427925c5f02f
MaxSlouka <jmslouka@gmail.com>
2023-07-03 15:22:51 +0200

    create get endpoint personDetails, create new repo method, find by id

--- foxbook/src/main/java/com/gfa/foxbook/foxbook/controllers/ApiController.java
+++ foxbook/src/main/java/com/gfa/foxbook/foxbook/controllers/ApiController.java
@@ -1,7 +1,27 @@
 package com.gfa.foxbook.foxbook.controllers;
-
+import com.gfa.foxbook.foxbook.models.User;
+import com.gfa.foxbook.foxbook.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class ApiController {
+
+    private final UserService userService;
+
+
+    public ApiController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping("/people/{id}")
+    public ResponseEntity<?> personDetails(@RequestParam Long id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
 }
--- foxbook/src/main/java/com/gfa/foxbook/foxbook/services/UserService.java
+++ foxbook/src/main/java/com/gfa/foxbook/foxbook/services/UserService.java
@@ -1,4 +1,7 @@
 package com.gfa.foxbook.foxbook.services;
 
+import com.gfa.foxbook.foxbook.models.User;
+
 public interface UserService {
+    User findById(Long id);
 }
