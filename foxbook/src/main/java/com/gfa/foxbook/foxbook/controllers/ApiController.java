ec0f1ffbcfc941fdf378350c0d92e8d140259543
MaxSlouka <jmslouka@gmail.com>
2023-07-03 16:24:09 +0200

    create new endpoint /people, findAllUsers, add find all method

--- foxbook/src/main/java/com/gfa/foxbook/foxbook/controllers/ApiController.java
+++ foxbook/src/main/java/com/gfa/foxbook/foxbook/controllers/ApiController.java
@@ -1,25 +1,27 @@
 package com.gfa.foxbook.foxbook.controllers;
 import com.gfa.foxbook.foxbook.services.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1")
 public class ApiController {
 
     private final UserService userService;
 
 
-    public ApiController(UserService userService) {
-        this.userService = userService;
-    }
-
-
-
     @GetMapping("/people/{id}")
     public ResponseEntity<?> personDetails(@PathVariable Long id) {
         User user = userService.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+
+    @GetMapping("/people")
+    public ResponseEntity<?> getAllUsers(){
+        return ResponseEntity.ok().body(userService.findAll());
     }
 }
--- foxbook/src/main/java/com/gfa/foxbook/foxbook/services/UserService.java
+++ foxbook/src/main/java/com/gfa/foxbook/foxbook/services/UserService.java
@@ -2,6 +2,10 @@ package com.gfa.foxbook.foxbook.services;
 
 import org.springframework.security.core.userdetails.User;
 
+import java.util.List;
+
 public interface UserService {
     User findById(Long id);
+
+    List<User> findAll();
 }
