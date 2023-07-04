e159cd8f5846e2853ce6f9010d3df55679a6df8b
Max Nesazal <maxnesazal1492@gmail.com>
2023-07-04 10:31:49 +0200

    set up env variables and secret key

--- foxbook/src/main/java/com/gfa/foxbook/foxbook/security/SecurityConstants.java
+++ foxbook/src/main/java/com/gfa/foxbook/foxbook/security/SecurityConstants.java
@@ -3,5 +3,5 @@ package com.gfa.foxbook.foxbook.security;
 public class SecurityConstants {
     public static final long JWT_EXPIRATION_TIME = 1000 * 60 * 60 * 10; // 10 hours
 
-    public static final byte[] JWT_SECRET = "SecretKeyToGenJWTsdsadsadeffnuvbtribsbfdvfdisvriuenviruesnvirnsvruvnfdsivnfudisnvdfis".getBytes();
+    public static final byte[] JWT_SECRET = System.getenv("SECRET_KEY").getBytes();
 }
