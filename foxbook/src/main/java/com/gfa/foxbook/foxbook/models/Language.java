3b3c42f5d4a9bbda2cf0ba001ac1cfcb9b039c26
MaxSlouka <jmslouka@gmail.com>
2023-07-04 15:19:31 +0200

    rework lang + tech

--- foxbook/src/main/java/com/gfa/foxbook/foxbook/models/Language.java
+++ foxbook/src/main/java/com/gfa/foxbook/foxbook/models/Language.java
@@ -19,18 +19,99 @@ public class Language {
     private long id;
     private String name;
 
-    List<String> spokenLanguages() {
-        return List.of(
-        "Italian", "Persian (Farsi)", "Thai", "Gujarati", "Polish", "Ukrainian", "Malayalam", "Kannada", "Oriya (Odia)",
-                "Burmese", "Hakka", "Bhojpuri", "Tagalog", "Yoruba", "Maithili", "Uzbek", "Sindhi", "Amharic", "Fula",
-                "Romanian", "Oromo", "Igbo", "Azerbaijani", "Dutch", "Kurdish", "Malagasy", "Saraiki", "Nepali", "Sinhala",
-                "Czech", "Haitian Creole", "Hungarian", "Swedish", "Belarusian", "Shona", "Zulu", "Somali", "Tigrinya",
-                "Kinyarwanda", "Hmong", "Uighur", "Greek", "Czech", "Bulgarian", "Khmer (Cambodian)", "Kazakh", "Danish",
-                "Maltese", "Finnish", "Icelandic", "Slovak", "Lithuanian", "Slovenian", "Latvian", "Estonian", "Georgian",
-                "Hausa", "Macedonian", "Albanian", "Afrikaans", "Swahili", "Mongolian", "Armenian", "Hebrew", "Yiddish",
-                "Gujarati", "Assamese", "Kashmiri", "Konkani", "Maithili", "Manipuri", "Sindhi", "Tibetan", "Tulu", "Santali",
-                "Awadhi", "Magahi", "Chhattisgarhi", "Malvi", "Bhili");
-
+    public Language(String name) {
+        this.name = name;
     }
 
+    public static List<Language> spokenLanguages() {
+        return List.of(
+                new Language("Italian"),
+                new Language("Persian (Farsi)"),
+                new Language("Thai"),
+                new Language("Gujarati"),
+                new Language("Polish"),
+                new Language("Ukrainian"),
+                new Language("Malayalam"),
+                new Language("Kannada"),
+                new Language("Oriya (Odia)"),
+                new Language("Burmese"),
+                new Language("Hakka"),
+                new Language("Bhojpuri"),
+                new Language("Tagalog"),
+                new Language("Yoruba"),
+                new Language("Maithili"),
+                new Language("Uzbek"),
+                new Language("Sindhi"),
+                new Language("Amharic"),
+                new Language("Fula"),
+                new Language("Romanian"),
+                new Language("Oromo"),
+                new Language("Igbo"),
+                new Language("Azerbaijani"),
+                new Language("Dutch"),
+                new Language("Kurdish"),
+                new Language("Malagasy"),
+                new Language("Saraiki"),
+                new Language("Nepali"),
+                new Language("Sinhala"),
+                new Language("Czech"),
+                new Language("Haitian Creole"),
+                new Language("Hungarian"),
+                new Language("Swedish"),
+                new Language("Belarusian"),
+                new Language("Shona"),
+                new Language("Zulu"),
+                new Language("Somali"),
+                new Language("Tigrinya"),
+                new Language("Kinyarwanda"),
+                new Language("Hmong"),
+                new Language("Uighur"),
+                new Language("Greek"),
+                new Language("Czech"),
+                new Language("Bulgarian"),
+                new Language("Khmer (Cambodian)"),
+                new Language("Kazakh"),
+                new Language("Danish"),
+                new Language("Maltese"),
+                new Language("Finnish"),
+                new Language("Icelandic"),
+                new Language("Slovak"),
+                new Language("Lithuanian"),
+                new Language("Slovenian"),
+                new Language("Latvian"),
+                new Language("Estonian"),
+                new Language("Georgian"),
+                new Language("Hausa"),
+                new Language("Macedonian"),
+                new Language("Albanian"),
+                new Language("Afrikaans"),
+                new Language("Swahili"),
+                new Language("Mongolian"),
+                new Language("Armenian"),
+                new Language("Hebrew"),
+                new Language("Yiddish"),
+                new Language("Gujarati"),
+                new Language("Assamese"),
+                new Language("Kashmiri"),
+                new Language("Konkani"),
+                new Language("Maithili"),
+                new Language("Manipuri"),
+                new Language("Sindhi"),
+                new Language("Tibetan"),
+                new Language("Tulu"),
+                new Language("Santali"),
+                new Language("Awadhi"),
+                new Language("Magahi"),
+                new Language("Chhattisgarhi"),
+                new Language("Malvi"),
+                new Language("Bhili")
+        );
+    }
 }
+
+
+
+
+
+
+
