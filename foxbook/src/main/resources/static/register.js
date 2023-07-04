5f5cbb8a3e19df28d051dda33ce9a784eba5d26f
Michal Capoun <michal.capoun@gmail.com>
2023-07-04 10:31:22 +0200

    create register.html

---
+++ foxbook/src/main/resources/static/register.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const newPostForm = document.querySelector("form");
+    const message = document.querySelector(".message");
+    message.textContent = "";
+
+    newPostForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const task = Object.fromEntries(formData);
+
+        fetch("/registration", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(task)
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error("Error occurred!");
+                }
+            })
+            .then(data => {
+                console.log("Provided data: ");
+                console.log(data);
+            })
+            .catch(error => {
+                console.error("Error:", error);
+            });
+
+        newPostForm.reset();
+    });
+});
