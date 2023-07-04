e816e0fd4f7afaa734ca582b4e02c1cf9bcc52ce
MaxSlouka <jmslouka@gmail.com>
2023-07-04 16:20:04 +0200

    add test for reg.endpoint

--- foxbook/src/test/java/com/gfa/foxbook/foxbook/controllers/WebControllerTests.java
+++ foxbook/src/test/java/com/gfa/foxbook/foxbook/controllers/WebControllerTests.java
@@ -1,6 +1,7 @@
 package com.gfa.foxbook.foxbook.controllers;
 
 import com.gfa.foxbook.foxbook.services.UserService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @WebMvcTest(controllers = WebController.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -18,4 +21,12 @@ public class WebControllerTests {
 
     @MockBean
     private UserService userService;
+    @Test
+    public void registrationEndpoint_ReturnsRegistrationHtmlView() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/registration"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("registration.html"));
+    }
+
+
 }
