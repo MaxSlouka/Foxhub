006c63878562539dda917f0d747a6984da0e0ea4
MaxSlouka <jmslouka@gmail.com>
2023-07-04 13:33:50 +0200

    create soc.media list

--- foxbook/src/main/java/com/gfa/foxbook/foxbook/models/SocialMedia.java
+++ foxbook/src/main/java/com/gfa/foxbook/foxbook/models/SocialMedia.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Entity
 @NoArgsConstructor
@@ -16,4 +18,14 @@ public class SocialMedia {
     private long id;
     private String name;
     private String url;
+
+    public static List<SocialMedia> getSocialMediaList() {
+        return List.of(
+                new SocialMedia(1, "GitHub", "https://github.com"),
+                new SocialMedia(2, "Facebook", "https://facebook.com"),
+                new SocialMedia(3, "Instagram", "https://instagram.com"),
+                new SocialMedia(4, "LinkedIn", "https://linkedin.com"),
+                new SocialMedia(5, "Twitter", "https://twitter.com")
+        );
+    }
 }
