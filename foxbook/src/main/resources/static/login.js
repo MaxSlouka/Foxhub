982171af3523af596dd0722980be0a9b2780a932
Max Nesazal <maxnesazal1492@gmail.com>
2023-07-05 12:58:41 +0200

    fixed bug in login

--- foxbook/src/main/resources/static/login.js
+++ foxbook/src/main/resources/static/login.js
@@ -1,38 +1,35 @@
+const newPostForm = document.querySelector("form");
+const message = document.querySelector(".message");
 
-document.addEventListener("DOMContentLoaded", () => {
+newPostForm.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-    const newPostForm = document.querySelector("form");
-    const message = document.querySelector(".message");
-    message.textContent = "";
+    const formData = new FormData(event.target);
+    const task = Object.fromEntries(formData);
+    console.log(task);
 
-    newPostForm.addEventListener("submit", (event) => {
-        event.preventDefault();
-
-        const formData = new FormData(event.target);
-        const task = Object.fromEntries(formData);
-
-        fetch("/authentication", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(task)
+    fetch("/api/v1/auth/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(task)
+    })
+        .then(response => {
+            if (response.ok) {
+                console.log("Response status: " + response.status);
+                return response.json();
+            } else {
+                throw new Error("Error occurred!");
+            }
         })
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error("Error occurred!");
-                }
-            })
-            .then(data => {
-                console.log("Provided data: ");
-                console.log(data);
-            })
-            .catch(error => {
-                console.error("Error:", error);
-            });
+        .then(data => {
+            console.log("Provided data: ");
+            console.log(data);
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
 
-        newPostForm.reset();
-    });
+    newPostForm.reset();
 });
