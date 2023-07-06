facbb94b53986d87d7adaa8503467a270cca85f0
TomasMacoun <macount@seznam.cz>
2023-07-06 15:41:43 +0200

    Completed testSaveSocialMedia

--- foxbook/src/test/java/com/gfa/foxbook/foxbook/repositories/SocialMediaRepositoryTests.java
+++ foxbook/src/test/java/com/gfa/foxbook/foxbook/repositories/SocialMediaRepositoryTests.java
@@ -1,5 +1,8 @@
 package com.gfa.foxbook.foxbook.repositories;
 
+import com.gfa.foxbook.foxbook.models.SocialMedia;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -7,8 +10,18 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 @DataJpaTest
 @AutoConfigureTestDatabase // there is problem with DB connection - need to fix that
 public class SocialMediaRepositoryTests {
-    
+
     @Autowired
     private SocialMediaRepository socialMediaRepository;
 
+    @Test
+    public void testSaveSocialMedia() {
+        SocialMedia socialMedia = new SocialMedia("Test-Social-Media", "https://test-social-media.com");
+
+        SocialMedia savedSocialMedia = socialMediaRepository.save(socialMedia);
+
+        Assertions.assertThat(savedSocialMedia.getId()).isNotNull();
+        Assertions.assertThat(savedSocialMedia.getName()).isEqualTo("Test-Social-Media");
+        Assertions.assertThat(savedSocialMedia.getUrl()).isEqualTo("https://test-social-media.com");
+    }
 }
