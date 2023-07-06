be2fa00e302aba150a6022768e237aeaaeb7ccdd
TomasMacoun <macount@seznam.cz>
2023-07-06 18:04:58 +0200

    Initial commit

---
+++ foxbook/src/test/java/com/gfa/foxbook/foxbook/services/SecurityServiceTests.java
@@ -0,0 +1,35 @@
+package com.gfa.foxbook.foxbook.services;
+import com.gfa.foxbook.foxbook.models.Role;
+import com.gfa.foxbook.foxbook.models.User;
+import com.gfa.foxbook.foxbook.models.dtos.security.RegisterDto;
+import com.gfa.foxbook.foxbook.repositories.RoleRepository;
+import com.gfa.foxbook.foxbook.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+public class SecurityServiceTests {
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private RoleRepository roleRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private SecurityService securityService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+}
