09b72ce20526e36cacc2aff1609b5da7af7677fd
TomasMacoun <macount@seznam.cz>
2023-07-06 15:22:00 +0200

    Completed testFindByName

--- foxbook/src/test/java/com/gfa/foxbook/foxbook/repositories/RoleRepositoryTests.java
+++ foxbook/src/test/java/com/gfa/foxbook/foxbook/repositories/RoleRepositoryTests.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Optional;
+
 @DataJpaTest
 @AutoConfigureTestDatabase
 public class RoleRepositoryTests {
@@ -23,5 +25,17 @@ public class RoleRepositoryTests {
 
         Assertions.assertThat(savedRole.getId()).isNotNull();
         Assertions.assertThat(savedRole.getName()).isEqualTo("TEST_ROLE");
+    }
+
+    @Test
+    public void testFindByName() {
+        Role role = new Role("TEST_ROLE");
+        roleRepository.save(role);
+
+        Optional<Role> optionalRole = roleRepository.findByName("TEST_ROLE");
+
+        Assertions.assertThat(optionalRole).isPresent();
+        Role foundRole = optionalRole.get();
+        Assertions.assertThat(foundRole.getName()).isEqualTo("TEST_ROLE");
     }
 }
