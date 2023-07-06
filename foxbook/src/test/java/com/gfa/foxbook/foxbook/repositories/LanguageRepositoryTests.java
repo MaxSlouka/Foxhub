8d81cc501751e0551943efe741f09ba1134cea85
TomasMacoun <macount@seznam.cz>
2023-07-06 15:34:52 +0200

    Added comment

--- foxbook/src/test/java/com/gfa/foxbook/foxbook/repositories/LanguageRepositoryTests.java
+++ foxbook/src/test/java/com/gfa/foxbook/foxbook/repositories/LanguageRepositoryTests.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @DataJpaTest
-@AutoConfigureTestDatabase
+@AutoConfigureTestDatabase // there is problem with DB connection - need to fix this
 public class LanguageRepositoryTests {
 
     @Autowired
