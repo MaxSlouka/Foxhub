478e9441b90bdfcb509a06641713f392dad53acd
TomasMacoun <macount@seznam.cz>
2023-07-06 15:42:35 +0200

    Completed testGetSocialMedia

--- foxbook/src/test/java/com/gfa/foxbook/foxbook/repositories/SocialMediaRepositoryTests.java
+++ foxbook/src/test/java/com/gfa/foxbook/foxbook/repositories/SocialMediaRepositoryTests.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Optional;
+
 @DataJpaTest
 @AutoConfigureTestDatabase // there is problem with DB connection - need to fix that
 public class SocialMediaRepositoryTests {
@@ -23,5 +25,18 @@ public class SocialMediaRepositoryTests {
         Assertions.assertThat(savedSocialMedia.getId()).isNotNull();
         Assertions.assertThat(savedSocialMedia.getName()).isEqualTo("Test-Social-Media");
         Assertions.assertThat(savedSocialMedia.getUrl()).isEqualTo("https://test-social-media.com");
+    }
+
+    @Test
+    public void testGetSocialMedia() {
+        SocialMedia socialMedia = new SocialMedia("Test-Social-Media", "https://test-social-media.com");
+        SocialMedia savedSocialMedia = socialMediaRepository.save(socialMedia);
+
+        Optional<SocialMedia> optionalSocialMedia = socialMediaRepository.findById(savedSocialMedia.getId());
+
+        Assertions.assertThat(optionalSocialMedia).isPresent();
+        SocialMedia foundSocialMedia = optionalSocialMedia.get();
+        Assertions.assertThat(foundSocialMedia.getName()).isEqualTo("Test-Social-Media");
+        Assertions.assertThat(foundSocialMedia.getUrl()).isEqualTo("https://test-social-media.com");
     }
 }
