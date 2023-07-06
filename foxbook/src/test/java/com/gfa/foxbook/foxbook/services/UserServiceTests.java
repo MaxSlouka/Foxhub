51cff31594f54ab8b892cfb78a3578a95e69bddd
TomasMacoun <macount@seznam.cz>
2023-07-06 17:43:43 +0200

    Completed testFindById

--- foxbook/src/test/java/com/gfa/foxbook/foxbook/services/UserServiceTests.java
+++ foxbook/src/test/java/com/gfa/foxbook/foxbook/services/UserServiceTests.java
@@ -1,4 +1,35 @@
 package com.gfa.foxbook.foxbook.services;
 
+import com.gfa.foxbook.foxbook.models.User;
+import com.gfa.foxbook.foxbook.repositories.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTests {
+    @Mock
+    private UserRepository userRepository;
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @Test
+    public void testFindById() {
+        User user = new User();
+        user.setId(1L);
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+
+        Optional<User> result = userService.findById(1L);
+
+        assertThat(result).isEqualTo(Optional.of(user));
+        verify(userRepository, times(1)).findById(1L);
+        verifyNoMoreInteractions(userRepository);
+    }
 }
