5ff4c25b8937a727e89e942c804137b2de88e6e6
TomasMacoun <macount@seznam.cz>
2023-07-06 15:52:42 +0200

    Completed testSaveTechnology

--- foxbook/src/test/java/com/gfa/foxbook/foxbook/repositories/TechnologyRepositoryTests.java
+++ foxbook/src/test/java/com/gfa/foxbook/foxbook/repositories/TechnologyRepositoryTests.java
@@ -1,5 +1,8 @@
 package com.gfa.foxbook.foxbook.repositories;
 
+import com.gfa.foxbook.foxbook.models.Technology;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -11,5 +14,13 @@ public class TechnologyRepositoryTests {
     @Autowired
     private TechnologyRepository technologyRepository;
 
+    @Test
+    public void testSaveTechnology() {
+        Technology technology = new Technology("TestTechnology");
 
+        Technology savedTechnology = technologyRepository.save(technology);
+
+        Assertions.assertThat(savedTechnology.getId()).isNotNull();
+        Assertions.assertThat(savedTechnology.getName()).isEqualTo("TestTechnology");
+    }
 }
