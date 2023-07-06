8cf9490f1a8411dc25d2a9b99a9d93cf4f710d12
TomasMacoun <macount@seznam.cz>
2023-07-06 15:34:06 +0200

    Completed LanguageRepositoryTests class

--- foxbook/src/test/java/com/gfa/foxbook/foxbook/repositories/LanguageRepositoryTests.java
+++ foxbook/src/test/java/com/gfa/foxbook/foxbook/repositories/LanguageRepositoryTests.java
@@ -3,6 +3,7 @@ package com.gfa.foxbook.foxbook.repositories;
 import com.gfa.foxbook.foxbook.models.Language;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
@@ -13,6 +14,7 @@ import java.util.Optional;
 @AutoConfigureTestDatabase
 public class LanguageRepositoryTests {
 
+    @Autowired
     private LanguageRepository languageRepository;
 
     @Test
