6a6e9b4e9140a2475dd0bb39a2193319c3930290
TomasMacoun <macount@seznam.cz>
2023-07-06 15:31:04 +0200

    Completed testSaveLanguage

--- foxbook/src/test/java/com/gfa/foxbook/foxbook/repositories/LanguageRepositoryTests.java
+++ foxbook/src/test/java/com/gfa/foxbook/foxbook/repositories/LanguageRepositoryTests.java
@@ -1,5 +1,8 @@
 package com.gfa.foxbook.foxbook.repositories;
 
+import com.gfa.foxbook.foxbook.models.Language;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
@@ -9,4 +12,13 @@ public class LanguageRepositoryTests {
 
     private LanguageRepository languageRepository;
 
+    @Test
+    public void testSaveLanguage() {
+        Language language = new Language("TEST_LANGUAGE");
+
+        Language savedLanguage = languageRepository.save(language);
+
+        Assertions.assertThat(savedLanguage.getId()).isNotNull();
+        Assertions.assertThat(savedLanguage.getName()).isEqualTo("TEST_LANGUAGE");
+    }
 }
