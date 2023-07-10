081824f652173281fd0015f0ff64c4afe854a970
MaxSlouka <jmslouka@gmail.com>
2023-07-10 17:07:25 +0200

    add parameter to impl

--- backend/src/main/java/com/gfa/foxbook/foxbook/services/LikeServiceImpl.java
+++ backend/src/main/java/com/gfa/foxbook/foxbook/services/LikeServiceImpl.java
@@ -14,16 +14,19 @@ public class LikeServiceImpl  implements LikeService{
 
     @Override
     public void like(Long postId, Long userId) {
+        assert likeRepository != null;
         likeRepository.like(postId, userId);
     }
 
     @Override
     public boolean voted(Long postId, Long userId) {
+        assert likeRepository != null;
         return likeRepository.voted(postId, userId);
     }
 
     @Override
     public int countLikes(Long postId) {
+        assert likeRepository != null;
         return likeRepository.countLikes(postId);
     }
 }
