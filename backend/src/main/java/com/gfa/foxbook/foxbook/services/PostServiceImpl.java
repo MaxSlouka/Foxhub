038d87c9cdea238d81d47f0f21a779f740a20578
MaxSlouka <jmslouka@gmail.com>
2023-07-10 17:09:44 +0200

    add not null

--- backend/src/main/java/com/gfa/foxbook/foxbook/services/PostServiceImpl.java
+++ backend/src/main/java/com/gfa/foxbook/foxbook/services/PostServiceImpl.java
@@ -15,41 +15,49 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public Post save(Post post) {
+        assert PostRepository != null;
         return PostRepository.save(post);
     }
 
     @Override
     public Post findById(Long id) {
+        assert PostRepository != null;
         return PostRepository.findById(id).orElse(null);
     }
 
     @Override
     public void delete(Post post) {
+        assert PostRepository != null;
         PostRepository.delete(post);
     }
 
     @Override
     public Iterable<Post> findAll() {
+        assert PostRepository != null;
         return PostRepository.findAll();
     }
 
     @Override
     public Iterable<Post> findAllByOrderByTimestampDesc() {
+        assert PostRepository != null;
         return PostRepository.findAllByOrderByTimestampDesc();
     }
 
     @Override
     public Iterable<Post> findAllByOrderByTimestampAsc() {
+        assert PostRepository != null;
         return PostRepository.findAllByOrderByTimestampAsc();
     }
 
     @Override
     public Iterable<Post> findAllByOrderByLikesDesc() {
+        assert PostRepository != null;
         return PostRepository.findAllByOrderByLikesDesc();
     }
 
         @Override
         public Iterable<Post> findByUserName(String authorName){
+            assert PostRepository != null;
             return PostRepository.findByUserName(authorName);
         }
     }
