77e5796166e6e498e26930e82a724f92c000fc66
DeenO37 <deen.bds@gmail.com>
2023-07-10 13:55:15 +0200

    add routes to router

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {MainPageComponent} from "./main-page/main-page.component";
+import {PeoplePageComponent} from "./people-page/people-page.component";
+import {ProfilePageComponent} from "./profile-page/profile-page.component";
+import {RegisterComponent} from "./register/register.component";
 
 const routes: Routes = [
-
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent},
+  { path: 'main', component: MainPageComponent },
+  { path: 'people', component: PeoplePageComponent},
+  { path: 'profile', component: ProfilePageComponent}
 ];
 
 @NgModule({
