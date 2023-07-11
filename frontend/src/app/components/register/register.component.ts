8d6203e8a233cbfa97c34b60de7a076c294a5ed2
TomasMacoun <macount@seznam.cz>
2023-07-11 10:21:57 +0200

    Update register.component.ts

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,19 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../_services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
-  formData = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: ''
+export class RegisterComponent implements OnInit {
+  form: any = {
+    firstName: null,
+    lastName: null,
+    email: null,
+    password: null
   };
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
 
-  onSubmit() {
-    console.log(this.formData);
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    const { firstName, lastName, email, password } = this.form;
+
+    this.authService.register(firstName, lastName, email, password).subscribe({
+      next: data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      error: err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    });
   }
 }
