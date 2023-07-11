a53dac879034fe42a90b20baa1fe702de3490c06
TomasMacoun <macount@seznam.cz>
2023-07-11 10:00:01 +0200

    refactored user.service.ts

--- frontend/src/app/_services/user.service.ts
+++ frontend/src/app/_services/user.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
 const API_URL = 'http://localhost:8080/api/v1/'
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class UserService {
 
-  constructor(private http:HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  getPublicContent():Observable<any> {
-    return this.http.get(API_URL + '....', {responseType: "text"})
+  getPublicContent(): Observable<any> {
+    return this.http.get(API_URL + '....', { responseType: "text" })
     // needs to be edited
   }
-  getUserContent():Observable<any> {
-    return this.http.get(API_URL + '....', {responseType: "json"})
+  
+  getUserContent(): Observable<any> {
+    return this.http.get(API_URL + '....', { responseType: "json" })
   }
 }
