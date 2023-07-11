36ea90f2aecfff71f46834ce15c220626d08f74c
MaxSlouka <jmslouka@gmail.com>
2023-07-11 10:25:57 +0200

    remove noargs, change it to list

--- backend/src/main/java/com/gfa/foxbook/foxbook/repositories/PostRepository.java
+++ backend/src/main/java/com/gfa/foxbook/foxbook/repositories/PostRepository.java
@@ -2,7 +2,9 @@ package com.gfa.foxbook.foxbook.repositories;
 
 import com.gfa.foxbook.foxbook.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     Iterable<Post> findAllByOrderByTimestampDesc();
 
--- backend/src/main/java/com/gfa/foxbook/foxbook/repositories/TechnologyRepository.java
+++ backend/src/main/java/com/gfa/foxbook/foxbook/repositories/TechnologyRepository.java
@@ -2,6 +2,8 @@ package com.gfa.foxbook.foxbook.repositories;
 
 import com.gfa.foxbook.foxbook.models.Technology;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TechnologyRepository extends JpaRepository<Technology, Long> {
 }
