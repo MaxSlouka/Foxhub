346e14d6a32b4ad830c7462ccbbce398af267957
maxnesss <maxnesazal1492@gmail.com>
2023-07-11 16:22:26 +0200

    add basic endpoints

--- backend/src/main/java/com/gfa/foxbook/foxbook/controllers/AdminController.java
+++ backend/src/main/java/com/gfa/foxbook/foxbook/controllers/AdminController.java
@@ -1,11 +1,22 @@
 package com.gfa.foxbook.foxbook.controllers;
 
+import com.gfa.foxbook.foxbook.models.User;
+import com.gfa.foxbook.foxbook.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("api/v1/admin")
 public class AdminController {
+    private final UserService userService;
+
+
+
 }
--- backend/src/main/java/com/gfa/foxbook/foxbook/controllers/PublicController.java
+++ backend/src/main/java/com/gfa/foxbook/foxbook/controllers/PublicController.java
@@ -1,14 +1,15 @@
 package com.gfa.foxbook.foxbook.controllers;
 
-import com.gfa.foxbook.foxbook.security.JWTGenerator;
-import com.gfa.foxbook.foxbook.services.SecurityService;
+import com.gfa.foxbook.foxbook.models.User;
 import com.gfa.foxbook.foxbook.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,5 +20,14 @@ public class PublicController {
     @GetMapping("/people")
     public ResponseEntity<?> getAllUsers() {
         return ResponseEntity.ok().body(userService.findAll());
+    }
+    @GetMapping("/person/{nickname}")
+    public ResponseEntity<?> personDetails(@PathVariable String nickname) {
+        Optional<User> maybeUser = userService.findByNickname(nickname);
+        if (maybeUser.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        User user = maybeUser.get();
+        return ResponseEntity.ok(user);
     }
 }
