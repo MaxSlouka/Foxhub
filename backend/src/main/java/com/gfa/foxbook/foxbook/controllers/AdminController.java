93227c1c920585c0c903afcc341b124b5f088d32
maxnesss <maxnesazal1492@gmail.com>
2023-07-11 16:23:22 +0200

    add basic endpoints

--- backend/src/main/java/com/gfa/foxbook/foxbook/controllers/AdminController.java
+++ backend/src/main/java/com/gfa/foxbook/foxbook/controllers/AdminController.java
@@ -5,9 +5,7 @@ import com.gfa.foxbook.foxbook.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -17,6 +15,29 @@ import java.util.Optional;
 public class AdminController {
     private final UserService userService;
 
+    @GetMapping("/posts")
+    public ResponseEntity<?> getAllPosts() {
+        return ResponseEntity.ok().build();
+    }
 
+    @GetMapping("/posts/{id}")
+    public ResponseEntity<?> getPostById(@PathVariable Long id) {
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/posts")
+    public ResponseEntity<?> createPost(@RequestBody Object object) {
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/posts/{id}")
+    public ResponseEntity<?> updatePost(@PathVariable Long id, @RequestBody Object object) {
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/posts/{id}")
+    public ResponseEntity<?> deletePost(@PathVariable Long id) {
+        return ResponseEntity.ok().build();
+    }
 
 }
