e0dc15c2055b6db9ee6bffc29a5ed48940b47e8c
maxnesss <maxnesazal1492@gmail.com>
2023-07-11 12:18:17 +0200

    bug fix

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,16 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import { ProfilePageComponent } from './profile-page/profile-page.component';
-import { PeoplePageComponent } from './people-page/people-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { ProfilePageComponent } from './components/profile-page/profile-page.component';
+import { PeoplePageComponent } from './components/people-page/people-page.component';
 import {NgOptimizedImage} from "@angular/common";
-import { IntroComponent } from './intro/intro.component';
-import { CreatePostComponent } from './main-page/post/create-post/create-post.component';
+import { IntroComponent } from './components/intro/intro.component';
+import {httpInterceptorProviders} from "./_helpers/interceptor";
 
 @NgModule({
   declarations: [
@@ -25,7 +25,6 @@ import { CreatePostComponent } from './main-page/post/create-post/create-post.co
     MainPageComponent,
     ProfilePageComponent,
     PeoplePageComponent,
-    CreatePostComponent,
     IntroComponent
   ],
   imports: [
