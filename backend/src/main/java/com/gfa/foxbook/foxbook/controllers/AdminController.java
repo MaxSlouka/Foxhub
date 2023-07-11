5e7bee88cc598cde35dd7b817569a4430a4931b6
maxnesss <maxnesazal1492@gmail.com>
2023-07-11 16:40:29 +0200

    remove api controller - not needed

--- backend/src/main/java/com/gfa/foxbook/foxbook/controllers/AdminController.java
+++ backend/src/main/java/com/gfa/foxbook/foxbook/controllers/AdminController.java
@@ -1,25 +1,14 @@
 package com.gfa.foxbook.foxbook.controllers;
 
-import com.gfa.foxbook.foxbook.models.User;
-import com.gfa.foxbook.foxbook.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("api/v1/admin")
 public class AdminController {
-    private final UserService userService;
-
-    @GetMapping("/posts")
-    public ResponseEntity<?> getAllPosts() {
-        return ResponseEntity.ok().build();
-    }
-
     @GetMapping("/posts/{id}")
     public ResponseEntity<?> getPostById(@PathVariable Long id) {
         return ResponseEntity.ok().build();
