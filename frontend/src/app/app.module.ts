5c20f93fbc1804e4af00e67e566bb47b3a315aa5
maxnesss <maxnesazal1492@gmail.com>
2023-07-11 09:12:03 +0200

    create components folder and move components into it for org reasons

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,15 +4,15 @@ import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import { ProfilePageComponent } from './profile-page/profile-page.component';
-import { PeoplePageComponent } from './people-page/people-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { ProfilePageComponent } from './components/profile-page/profile-page.component';
+import { PeoplePageComponent } from './components/people-page/people-page.component';
 import {NgOptimizedImage} from "@angular/common";
-import { IntroComponent } from './intro/intro.component';
+import { IntroComponent } from './components/intro/intro.component';
 import {httpInterceptorProviders} from "./_helpers/interceptor";
 
 @NgModule({
