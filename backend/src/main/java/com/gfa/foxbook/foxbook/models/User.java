ab5b2db816076824005793e0c55be25bc04c282c
DeenO37 <deen.bds@gmail.com>
2023-07-12 16:07:59 +0200

    add 2 components + backend changes

--- backend/src/main/java/com/gfa/foxbook/foxbook/models/User.java
+++ backend/src/main/java/com/gfa/foxbook/foxbook/models/User.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.w3c.dom.Text;
 
 import java.util.Date;
 import java.util.List;
--- frontend/src/app/components/social-media/social-media.component.ts
+++ frontend/src/app/components/social-media/social-media.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {SocialMedia} from "../../models/socialMedia";
 
 @Component({
   selector: 'app-social-media',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./social-media.component.css']
 })
 export class SocialMediaComponent {
-
+// @ts-ignore
+  @Input() social: SocialMedia;
 }
