15334a76d2025ead4d596cdd4da352e1572e078c
DeenO37 <deen.bds@gmail.com>
2023-07-12 11:39:02 +0200

    add people to profile anchor func

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from "./components/login/login.component";
 import {MainPageComponent} from "./components/main-page/main-page.component";
 import {PeoplePageComponent} from "./components/people-page/people-page.component";
@@ -8,17 +8,18 @@ import {RegisterComponent} from "./components/register/register.component";
 import {IntroComponent} from "./components/intro/intro.component";
 
 const routes: Routes = [
-  { path: '', component: IntroComponent},
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent},
-  { path: 'main', component: MainPageComponent },
-  { path: 'people', component: PeoplePageComponent},
-  {path: 'profile', component:ProfilePageComponent},
-  { path: 'profile/:username', component: ProfilePageComponent}
+  {path: '', component: IntroComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'main', component: MainPageComponent},
+  {path: 'people', component: PeoplePageComponent},
+  {path: 'profile', component: ProfilePageComponent},
+  {path: 'profile/:username', component: ProfilePageComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
