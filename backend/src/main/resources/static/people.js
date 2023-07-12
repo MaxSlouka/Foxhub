fa126d3717765ddf2b84c8dbe9899be88a284389
maxnesss <maxnesazal1492@gmail.com>
2023-07-12 16:05:54 +0200

    remove temporary frontend

--- backend/src/main/resources/static/people.js
+++
@@ -1,9 +0,0 @@
-fetch('/api/people')
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(person => {
-            const li = document.createElement('li');
-            li.innerHTML = `${person.firstName} ${person.lastName} (${person.email})`;
-            document.querySelector('.list-of-people').appendChild(li);
-        });
-    });
