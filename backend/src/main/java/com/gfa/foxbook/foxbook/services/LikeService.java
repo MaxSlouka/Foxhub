03ef31fc6689849d651d9a22d641b7924e0f48bf
MaxSlouka <jmslouka@gmail.com>
2023-07-14 11:24:42 +0200

    add new methods

--- backend/src/main/java/com/gfa/foxbook/foxbook/services/LikeService.java
+++ backend/src/main/java/com/gfa/foxbook/foxbook/services/LikeService.java
@@ -1,6 +1,17 @@
 package com.gfa.foxbook.foxbook.services;
 
+import com.gfa.foxbook.foxbook.models.Like;
+import com.gfa.foxbook.foxbook.models.User;
+
 public interface LikeService {
     void like(Long postId, Long userId);
-    boolean hasUserLiked(Long postId, Long userId);
+    boolean hasUserLikedPost(Long postId, Long userId);
+    public User getUser(Long userId);
+
+    void save(Like like);
+
+    boolean hasUserLiked(Long commentId, Long userId);
+    void likePost(Long postId, Long userId);
+
+    void like(Like like);
 }
