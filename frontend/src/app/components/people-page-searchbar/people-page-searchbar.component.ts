1c31a72520cebb099c5c7f7aa611d5edda8a2f48
DeenO37 <deen.bds@gmail.com>
2023-07-14 10:13:52 +0200

    add search by technology functionality

--- frontend/src/app/components/people-page-searchbar/people-page-searchbar.component.ts
+++ frontend/src/app/components/people-page-searchbar/people-page-searchbar.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {User} from "../../models/user";
+import {Technology} from "../../models/technology";
 
 
 @Component({
@@ -11,17 +12,26 @@ export class PeoplePageSearchbarComponent {
 // @ts-ignore
   @Input() users: User[];
   // @ts-ignore
-  @Input() fullUsers:User[];
+  @Input() fullUsers: User[];
+
   public searchUser(key: string): void {
     let results: User[] = [];
     for (const user of this.users) {
+
+      // @ts-ignore
+      for(const technology of user.technologies){
+        if(technology.name.toLowerCase().indexOf(key.toLowerCase()) !== -1){
+          results.push(user);
+        }
+      }
       if (user.firstName.toLowerCase().indexOf(key.toLowerCase()) !== -1
-        || user.lastName.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
+        || user.lastName.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      ) {
         results.push(user);
       }
     }
     this.users = results;
-    if(!key){
+    if (!key) {
       this.users = this.fullUsers
     }
   }
