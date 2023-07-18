aa59532a5bb1258aac5bcc226d527916f85ca915
MaxSlouka <jmslouka@gmail.com>
2023-07-18 12:32:07 +0200

    create QR code generator

--- frontend/src/app/components/profile-page/profile-page.component.ts
+++ frontend/src/app/components/profile-page/profile-page.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {User} from "../../models/user";
 import {ProfileService} from "../../_services/profile.service";
 import {ActivatedRoute} from '@angular/router';
+import {SafeUrl, SafeValue} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-profile-page',
@@ -13,14 +14,26 @@ export class ProfilePageComponent implements OnInit{
   username: string | null = "";
   // @ts-ignore
   user: User ;
+  qrCodeDownloadLink: SafeValue = "";
 
   constructor(private profileService: ProfileService,
-              private activatedroute: ActivatedRoute) {
+              private activatedroute: ActivatedRoute,
+              ) {
   }
 
   ngOnInit() {
     this.username = this.activatedroute.snapshot.paramMap.get("username");
     this.profileService.getUser(this.username)
       .subscribe(user => this.user = user)
+    // @ts-ignore
+    this.onChangeURL();
+
   }
+
+  onChangeURL(url: SafeValue) {
+    let qrCodeURL = 'http://localhost:4200/profile/' + this.username;
+    this.qrCodeDownloadLink = url;
+
+
+    }
 }
