f668036f9f11a0693785d1bbf769077d6d8b84fa
MaxSlouka <jmslouka@gmail.com>
2023-07-20 10:01:43 +0200

    create cookie popup

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { ApiService } from "./_services/api/api.service"
-import { DataService } from "./_services/api/data.service";
+import {Component} from '@angular/core';
+import {ApiService} from "./_services/api/api.service";
+import {DataService} from "./_services/api/data.service";
+import {CookieService} from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-root',
@@ -10,14 +11,43 @@ import { DataService } from "./_services/api/data.service";
 
 export class AppComponent {
   title = 'frontend';
+  public cookie_name = '';
+  public all_cookies: any = '';
+  public showCookiePopup = false;
 
   // @ts-ignore
   constructor(private apiService: ApiService,
-    private dataService: DataService) {
+              private dataService: DataService,
+              private cookieService: CookieService) {
+  }
+
+  setCookie() {
+    this.cookieService.set('Foxbook', 'Welcome to Foxbook!');
+  }
+
+  deleteCookie() {
+    this.cookieService.delete('Foxbook');
+  }
+
+  deleteAll() {
+    this.cookieService.deleteAll();
+  }
+
+  acceptCookies() {
+    this.cookieService.set('cookie_consent', 'true');
+    this.showCookiePopup = false;
+  }
+
+  declineCookies() {
+    this.cookieService.set('cookie_consent', 'false');
+    this.showCookiePopup = false;
   }
 
   ngOnInit(): void {
     this.getUsers();
+    this.cookie_name = this.cookieService.get('Foxbook');
+    this.all_cookies = this.cookieService.getAll();
+    this.showCookiePopup = !this.cookieService.check('cookie_consent');
   }
 
   private getUsers(): void {
@@ -25,6 +55,6 @@ export class AppComponent {
       .getAll().subscribe((users) => {
         this.dataService.users = users;
       }
-      );
+    );
   }
 }
--- frontend/src/app/components/intro/intro.component.ts
+++ frontend/src/app/components/intro/intro.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from "../../_services/auth.service";
 import { StorageService } from "../../_services/storage.service";
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-intro',
@@ -11,9 +12,11 @@ import { StorageService } from "../../_services/storage.service";
 export class IntroComponent implements OnInit {
 
   isLoggedIn = false;
+  public showCookiePopup = false;
 
   constructor(private authService: AuthService,
-    private storageService: StorageService) {
+              private storageService: StorageService,
+              private cookieService: CookieService) {
   }
 
   ngOnInit(): void {
@@ -21,5 +24,16 @@ export class IntroComponent implements OnInit {
       this.isLoggedIn = true;
       // this.roles = this.storageService.getUser().roles;
     }
+    this.showCookiePopup = !this.cookieService.check('cookie_consent');
+  }
+
+  acceptCookies() {
+    this.cookieService.set('cookie_consent', 'true');
+    this.showCookiePopup = false;
+  }
+
+  declineCookies() {
+    this.cookieService.set('cookie_consent', 'false');
+    this.showCookiePopup = false;
   }
 }
