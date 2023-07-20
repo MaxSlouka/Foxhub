2316dc3fd7fcb562a8db11ffda1f0537d0cc7950
maxnesss <maxnesazal1492@gmail.com>
2023-07-20 12:23:13 +0200

    Create laguage.component.ts

---
+++ frontend/src/app/components/language/laguage.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Language } from "../../models/language";
+
+@Component({
+  selector: 'app-laguage',
+  templateUrl: './laguage.component.html',
+  styleUrls: ['./laguage.component.css']
+})
+
+export class LaguageComponent {
+  // @ts-ignore
+  @Input() language: Language;
+}
