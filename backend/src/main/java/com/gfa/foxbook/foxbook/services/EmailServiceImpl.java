e84fa1a5fc9b2b741b61dcf1a0e0c9802931ff28
maxnesss <maxnesazal1492@gmail.com>
2023-07-20 12:38:33 +0200

    Update EmailServiceImpl.java

--- backend/src/main/java/com/gfa/foxbook/foxbook/services/EmailServiceImpl.java
+++ backend/src/main/java/com/gfa/foxbook/foxbook/services/EmailServiceImpl.java
@@ -2,6 +2,7 @@ package com.gfa.foxbook.foxbook.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +10,16 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class EmailServiceImpl {
     @Autowired
-private JavaMailSender mailSenderSender;
+private JavaMailSender mailSender;
+
+    public void send(String from, String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
+    }
 
 
 
