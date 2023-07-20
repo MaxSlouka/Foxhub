4f38bb5ea3001100177e9a4698e65be5eba6307b
maxnesss <maxnesazal1492@gmail.com>
2023-07-20 14:08:58 +0200

    Update EmailServiceImpl.java

--- backend/src/main/java/com/gfa/foxbook/foxbook/services/EmailServiceImpl.java
+++ backend/src/main/java/com/gfa/foxbook/foxbook/services/EmailServiceImpl.java
@@ -4,7 +4,6 @@ package com.gfa.foxbook.foxbook.services;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -25,21 +24,4 @@ private final JavaMailSender mailSender;
 
         mailSender.send(message);
     }
-
-//    public void sendWithAttach(String from, String to, String subject,
-//                               String text, String attachName,
-//                               InputStreamSource inputStream) throws MessagingException {
-//        MimeMessage message = mailSender.createMimeMessage();
-//        MimeMessageHelper helper = new MimeMessageHelper(message, true);
-//        helper.setFrom(from);
-//        helper.setTo(to);
-//        helper.setSubject(subject);
-//        helper.setText(text, true);
-//        helper.addAttachment(attachName, inputStream);
-//        mailSender.send(message);
-//    }
-
-
-
-
 }
