ab287e37aca485ecc343ac809091e78d3cf1cb69
DeenO37 <deen.bds@gmail.com>
2023-07-20 15:09:43 +0200

    add updated data loaders

--- backend/src/main/java/com/gfa/foxbook/foxbook/loaders/TechnologyDataLoader.java
+++ backend/src/main/java/com/gfa/foxbook/foxbook/loaders/TechnologyDataLoader.java
@@ -1,11 +1,13 @@
 package com.gfa.foxbook.foxbook.loaders;
 
-
 import com.gfa.foxbook.foxbook.models.nonusermodels.Technology;
 import com.gfa.foxbook.foxbook.repositories.TechnologyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class TechnologyDataLoader implements CommandLineRunner {
@@ -26,156 +28,80 @@ public class TechnologyDataLoader implements CommandLineRunner {
     }
 
     private void loadInitialTechnologies() {
-        // Add the initial technologies here
+        // List of technologies
+        List<String> technologiesList = Arrays.asList(
+                "HTML",
+                "CSS",
+                "JavaScript",
+                "TypeScript",
+                "React",
+                "Angular",
+                "Vue.js",
+                "Node.js",
+                "Express.js",
+                "Django",
+                "Flask",
+                "Ruby on Rails",
+                "Spring Boot",
+                "ASP.NET",
+                "Swift",
+                "Objective-C",
+                "Kotlin",
+                "Java (Android)",
+                "React Native",
+                "Flutter",
+                "Xamarin",
+                "Unity",
+                "Unreal Engine",
+                "MySQL",
+                "PostgreSQL",
+                "Oracle",
+                "Microsoft SQL Server",
+                "MongoDB",
+                "Redis",
+                "Cassandra",
+                "Amazon Web Services (AWS)",
+                "Microsoft Azure",
+                "Google Cloud Platform (GCP)",
+                "Docker",
+                "Kubernetes",
+                "Jenkins",
+                "Travis CI",
+                "CircleCI",
+                "TensorFlow",
+                "PyTorch",
+                "scikit-learn",
+                "Keras",
+                "Hadoop",
+                "Apache Spark",
+                "Apache Flink",
+                "Apache Kafka",
+                "Arduino",
+                "Raspberry Pi",
+                "Linux (Various distributions)",
+                "Windows",
+                "macOS",
+                "Git",
+                "GitHub",
+                "GitLab",
+                "Bitbucket",
+                "TCP/IP",
+                "HTTP/HTTPS",
+                "WebSocket",
+                "Bitcoin",
+                "Ethereum",
+                "Smart Contracts",
+                "Firewall",
+                "Encryption",
+                "SSL/TLS",
+                "Penetration Testing"
+        );
 
-        // HTML
-        Technology html = new Technology();
-        html.setName("HTML");
-        technologyRepository.save(html);
-
-        // CSS
-        Technology css = new Technology();
-        css.setName("CSS");
-        technologyRepository.save(css);
-
-        // JavaScript
-        Technology js = new Technology();
-        js.setName("JavaScript");
-        technologyRepository.save(js);
-
-        // Python
-        Technology python = new Technology();
-        python.setName("Python");
-        technologyRepository.save(python);
-
-        // Java
-        Technology java = new Technology();
-        java.setName("Java");
-        technologyRepository.save(java);
-
-        // PHP
-        Technology php = new Technology();
-        php.setName("PHP");
-        technologyRepository.save(php);
-
-        // Ruby
-        Technology ruby = new Technology();
-        ruby.setName("Ruby");
-        technologyRepository.save(ruby);
-
-        // TypeScript
-        Technology ts = new Technology();
-        ts.setName("TypeScript");
-        technologyRepository.save(ts);
-
-        // C#
-        Technology cSharp = new Technology();
-        cSharp.setName("C#");
-        technologyRepository.save(cSharp);
-
-        // Swift
-        Technology swift = new Technology();
-        swift.setName("Swift");
-        technologyRepository.save(swift);
-
-        // Go
-        Technology go = new Technology();
-        go.setName("Go");
-        technologyRepository.save(go);
-
-        // Kotlin
-        Technology kotlin = new Technology();
-        kotlin.setName("Kotlin");
-        technologyRepository.save(kotlin);
-
-        // Rust
-        Technology rust = new Technology();
-        rust.setName("Rust");
-        technologyRepository.save(rust);
-
-        // SQL
-        Technology sql = new Technology();
-        sql.setName("SQL");
-        technologyRepository.save(sql);
-
-        // React
-        Technology react = new Technology();
-        react.setName("React");
-        technologyRepository.save(react);
-
-        // Angular
-        Technology angular = new Technology();
-        angular.setName("Angular");
-        technologyRepository.save(angular);
-
-        // Vue.js
-        Technology vuejs = new Technology();
-        vuejs.setName("Vue.js");
-        technologyRepository.save(vuejs);
-
-        // Django
-        Technology django = new Technology();
-        django.setName("Django");
-        technologyRepository.save(django);
-
-        // Spring Boot
-        Technology springBoot = new Technology();
-        springBoot.setName("Spring Boot");
-        technologyRepository.save(springBoot);
-
-        // Ruby on Rails
-        Technology rails = new Technology();
-        rails.setName("Ruby on Rails");
-        technologyRepository.save(rails);
-
-        // Express.js
-        Technology express = new Technology();
-        express.setName("Express.js");
-        technologyRepository.save(express);
-
-        // Flask
-        Technology flask = new Technology();
-        flask.setName("Flask");
-        technologyRepository.save(flask);
-
-        // Laravel
-        Technology laravel = new Technology();
-        laravel.setName("Laravel");
-        technologyRepository.save(laravel);
-
-        // ASP.NET
-        Technology aspNet = new Technology();
-        aspNet.setName("ASP.NET");
-        technologyRepository.save(aspNet);
-
-        // Node.js
-        Technology nodejs = new Technology();
-        nodejs.setName("Node.js");
-        technologyRepository.save(nodejs);
-
-        // React Native
-        Technology reactNative = new Technology();
-        reactNative.setName("React Native");
-        technologyRepository.save(reactNative);
-
-        // Flutter
-        Technology flutter = new Technology();
-        flutter.setName("Flutter");
-        technologyRepository.save(flutter);
-
-        // Xamarin
-        Technology xamarin = new Technology();
-        xamarin.setName("Xamarin");
-        technologyRepository.save(xamarin);
-
-        // Unity
-        Technology unity = new Technology();
-        unity.setName("Unity");
-        technologyRepository.save(unity);
-
-        // Unreal Engine
-        Technology unrealEngine = new Technology();
-        unrealEngine.setName("Unreal Engine");
-        technologyRepository.save(unrealEngine);
+        // Save each technology to the repository
+        for (String tech : technologiesList) {
+            Technology technology = new Technology();
+            technology.setName(tech);
+            technologyRepository.save(technology);
+        }
     }
 }
