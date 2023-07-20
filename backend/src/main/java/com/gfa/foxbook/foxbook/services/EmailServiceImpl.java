994bec74848c4260fee62bff8a709322d3da75c2
maxnesss <maxnesazal1492@gmail.com>
2023-07-20 14:05:57 +0200

    Update EmailServiceImpl.java

--- backend/src/main/java/com/gfa/foxbook/foxbook/services/EmailServiceImpl.java
+++ backend/src/main/java/com/gfa/foxbook/foxbook/services/EmailServiceImpl.java
@@ -1,25 +1,44 @@
 package com.gfa.foxbook.foxbook.services;
 
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class EmailServiceImpl {
-    @Autowired
-private JavaMailSender mailSender;
+private final JavaMailSender mailSender;
 
-    public void send(String from, String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(from);
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
+    public void send(String from, String to, String subject, String text) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+                MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setFrom(from);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text, true);
+
         mailSender.send(message);
     }
+
+//    public void sendWithAttach(String from, String to, String subject,
+//                               String text, String attachName,
+//                               InputStreamSource inputStream) throws MessagingException {
+//        MimeMessage message = mailSender.createMimeMessage();
+//        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+//        helper.setFrom(from);
+//        helper.setTo(to);
+//        helper.setSubject(subject);
+//        helper.setText(text, true);
+//        helper.addAttachment(attachName, inputStream);
+//        mailSender.send(message);
+//    }
+
 
 
 
