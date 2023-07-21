5f273bf1ccacf70312cbaa2ba3205d9a25561e5c
maxnesss <maxnesazal1492@gmail.com>
2023-07-21 13:17:48 +0200

    Update change-password.component.ts

--- frontend/src/app/components/change-password/change-password.component.ts
+++ frontend/src/app/components/change-password/change-password.component.ts
@@ -7,10 +7,11 @@ import { Component } from '@angular/core';
 })
 export class ChangePasswordComponent {
   form: any = {
-    password: null
+    oldPassword: null,
+    newPassword:null
   };
 
   onSubmit() {
-    
+
   }
 }
