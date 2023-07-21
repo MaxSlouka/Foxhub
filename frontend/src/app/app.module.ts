226a2140b1d0a3fc357b119e7d3a1baef0f5ccdc
maxnesss <maxnesazal1492@gmail.com>
2023-07-21 11:03:25 +0200

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -24,7 +24,7 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 import { UserSettingsPageComponent } from './components/user-settings-page/user-settings-page.component';
 import { TermsOfUseComponent } from './components/terms-of-use/terms-of-use.component';
 import { HeaderSearchbarComponent } from './components/header-searchbar/header-searchbar.component';
-import { UpdatePasswordComponent } from './components/update-password/update-password.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 import { PostsComponent } from './components/posts/posts.component';
 import { PostComponent } from './components/post/post.component';
 import { PostFormComponent } from './components/post-form/post-form.component';
@@ -60,7 +60,7 @@ import { AsideSearchBarComponent } from './components/aside-search-bar/aside-sea
     UserSettingsPageComponent,
     TermsOfUseComponent,
     HeaderSearchbarComponent,
-    UpdatePasswordComponent,
+    ResetPasswordComponent,
     PostsComponent,
     PostComponent,
     PostFormComponent,
