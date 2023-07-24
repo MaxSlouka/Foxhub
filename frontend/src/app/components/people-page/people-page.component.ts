87da2d6a57696eaff4b3bef7dffc6957a63788ee
DeenO37 <deen.bds@gmail.com>
2023-07-24 13:08:31 +0200

    add used tech list

--- frontend/src/app/components/people-page/people-page.component.ts
+++ frontend/src/app/components/people-page/people-page.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from "../../_services/api/data.service";
 import {Technology} from "../../models/technology";
 import {TechnologyService} from "../../_services/technology.service";
+import {ApiService} from "../../_services/api/api.service";
+import {User} from "../../models/user";
 
 @Component({
   selector: 'app-people-page',
@@ -12,22 +14,39 @@ import {TechnologyService} from "../../_services/technology.service";
 export class PeoplePageComponent implements OnInit {
   technologies:Technology[] = [];
   selectedTechnologies:string[] = [];
+  usedTechnologies:Technology[] = [];
+  // @ts-ignore
+  users: User[];
 
-  constructor(public dataService: DataService, private technolgyService:TechnologyService) {
+  constructor(public dataService: DataService,
+              private technolgyService:TechnologyService,
+              private apiService:ApiService){
   }
 
   ngOnInit(): void {
     this.technolgyService.getAll().subscribe(technologies=>{
-      for (const technology of technologies) {
-        this.technologies.push(technology);
-      }
+       this.technologies = technologies;
+      this.usedTechnologiesList();
+    });
+    // @ts-ignore
+    this.apiService.getAll().subscribe(users=>{
+      this.users = users;
     });
   }
 
-  onSomthing() {
-    console.log(this.technologies);
+  // @ts-ignore
+  usedTechnologiesList(): Technology[] {
+    for(let user of this.users){
+      // @ts-ignore
+      for(let tech of user.technologies){
+        // @ts-ignore
+        if(!this.usedTechnologies.includes(tech)){
+          // @ts-ignore
+          this.usedTechnologies.push(tech);
+        }
+      }
+    }
   }
-
 
 
   addToTechList(event: any, tech: string) {
@@ -41,7 +60,7 @@ export class PeoplePageComponent implements OnInit {
         this.selectedTechnologies.splice(index, 1);
       }
     }
-    console.log(this.selectedTechnologies);
   }
+
 
 }
