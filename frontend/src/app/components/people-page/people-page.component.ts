24fdf0378860758b50f9716a16e09a09340c8303
DeenO37 <deen.bds@gmail.com>
2023-07-24 14:48:54 +0200

    add filter technology function

--- frontend/src/app/components/people-page/people-page.component.ts
+++ frontend/src/app/components/people-page/people-page.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService } from "../../_services/api/data.service";
+import {Component, OnInit} from '@angular/core';
+import {DataService} from "../../_services/api/data.service";
 import {Technology} from "../../models/technology";
 import {TechnologyService} from "../../_services/technology.service";
 import {ApiService} from "../../_services/api/api.service";
 import {User} from "../../models/user";
+
 
 @Component({
   selector: 'app-people-page',
@@ -12,41 +13,48 @@ import {User} from "../../models/user";
 })
 
 export class PeoplePageComponent implements OnInit {
-  technologies:Technology[] = [];
-  selectedTechnologies:string[] = [];
-  usedTechnologies:Technology[] = [];
-  // @ts-ignore
-  users: User[];
+  technologies: Technology[] = [];
+  selectedTechnologies: string[] = [];
+  usedTechnologies: Technology[] = [];
 
-  constructor(public dataService: DataService,
-              private technolgyService:TechnologyService,
-              private apiService:ApiService){
+
+  // @ts-ignore
+  users: User[] = [];
+  fullUsers: User[] = [];
+
+  constructor(
+              private technolgyService: TechnologyService,
+              private apiService: ApiService,
+              ){
   }
 
   ngOnInit(): void {
-    this.technolgyService.getAll().subscribe(technologies=>{
-       this.technologies = technologies;
+    this.technolgyService.getAll().subscribe(technologies => {
+      this.technologies = technologies;
       this.usedTechnologiesList();
     });
     // @ts-ignore
-    this.apiService.getAll().subscribe(users=>{
+    this.apiService.getAll().subscribe(users => {
       this.users = users;
+      this.fullUsers = users;
+      this.usedTechnologiesList();
     });
   }
 
   // @ts-ignore
   usedTechnologiesList(): Technology[] {
-    for(let user of this.users){
+    const usedTechnologies: Technology[] = [];
+    for (let user of this.users) {
       // @ts-ignore
-      for(let tech of user.technologies){
-        // @ts-ignore
-        if(!this.usedTechnologies.includes(tech)){
-          // @ts-ignore
-          this.usedTechnologies.push(tech);
+      for (let tech of user.technologies) {
+        if (!usedTechnologies.some(usedTech => usedTech.name === tech.name)) {
+          usedTechnologies.push(tech);
         }
       }
     }
+    return usedTechnologies;
   }
+
 
 
   addToTechList(event: any, tech: string) {
@@ -60,7 +68,23 @@ export class PeoplePageComponent implements OnInit {
         this.selectedTechnologies.splice(index, 1);
       }
     }
+    this.technologiesFilter(this.selectedTechnologies);
   }
 
 
+  technologiesFilter(keys: string[]) {
+    if (keys.length === 0) {
+      this.users = this.fullUsers;
+      return;
+    }
+
+    const lowerCaseKeys = keys.map(key => key.toLowerCase());
+    this.users = this.fullUsers.filter(user =>
+      lowerCaseKeys.every(key =>
+          user.technologies && user.technologies.some(technology =>
+            technology.name.toLowerCase().includes(key)
+          )
+      )
+    );
+  }
 }
