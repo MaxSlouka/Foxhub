4962dbeacaf44b49acea2233d7af680ebc8acc6d
DeenO37 <deen.bds@gmail.com>
2023-07-24 12:27:28 +0200

    used tech list

--- frontend/src/app/components/people-page/people-page.component.ts
+++ frontend/src/app/components/people-page/people-page.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService } from "../../_services/api/data.service";
+import {Component, OnInit} from '@angular/core';
+import {DataService} from "../../_services/api/data.service";
+import {Technology} from "../../models/technology";
+import {User} from "../../models/user";
 
 @Component({
   selector: 'app-people-page',
@@ -9,9 +11,36 @@ import { DataService } from "../../_services/api/data.service";
 
 export class PeoplePageComponent implements OnInit {
 
+  usedTechnologies: Technology[] = [];
+  // @ts-ignore
+  users: User[] ;
+
   constructor(public dataService: DataService) {
   }
 
   ngOnInit(): void {
+   if(this.dataService.users){
+     this.users = this.dataService.users;
+     usedTechnologiesList();
+     console.log(this.users);
+   }
   }
+}
+
+
+// @ts-ignore
+function usedTechnologiesList(): Technology[] {
+  // @ts-ignore
+  for(let user: User of this.users) {
+   // @ts-ignore
+    for(let technology: Technology of user.technologies) {
+     // @ts-ignore
+     if(!this.usedTechnologies.includes(technology)) {
+       // @ts-ignore
+       this.usedTechnologies.push(technology);
+     }
+   }
+  }
+  // @ts-ignore
+  console.log(this.usedTechnologies);
 }
