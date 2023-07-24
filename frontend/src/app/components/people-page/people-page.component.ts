bd2bee7535c2b4472fdf4a0bf7cfc9fb61921ebd
maxnesss <maxnesazal1492@gmail.com>
2023-07-24 14:58:16 +0200

    Update people-page.component.ts

--- frontend/src/app/components/people-page/people-page.component.ts
+++ frontend/src/app/components/people-page/people-page.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService } from "../../_services/api/data.service";
+import {Component, OnInit} from '@angular/core';
+import {DataService} from "../../_services/api/data.service";
 import {Technology} from "../../models/technology";
 import {TechnologyService} from "../../_services/technology.service";
 import {ApiService} from "../../_services/api/api.service";
 import {User} from "../../models/user";
+import {Language} from "../../models/language";
+import {LanguageService} from "../../_services/language.service";
 
 @Component({
   selector: 'app-people-page',
@@ -12,40 +14,64 @@ import {User} from "../../models/user";
 })
 
 export class PeoplePageComponent implements OnInit {
-  technologies:Technology[] = [];
-  selectedTechnologies:string[] = [];
-  usedTechnologies:Technology[] = [];
+  technologies: Technology[] = [];
+  languages: Language[] = [];
+  selectedTechnologies: string[] = [];
+  selectedLanguages: string[] = [];
+  usedTechnologies: Technology[] = [];
+  usedLanguages: Language[] = [];
+
   // @ts-ignore
   users: User[];
 
   constructor(public dataService: DataService,
-              private technolgyService:TechnologyService,
-              private apiService:ApiService){
+              private technologyService: TechnologyService,
+              private languageService: LanguageService,
+              private apiService: ApiService) {
   }
 
   ngOnInit(): void {
-    this.technolgyService.getAll().subscribe(technologies=>{
-       this.technologies = technologies;
-      this.usedTechnologiesList();
+    this.technologyService.getAll().subscribe(technologies => {
+      this.technologies = technologies;
     });
+    this.languageService.getAll().subscribe(languages => {
+      this.languages = languages;
+    })
     // @ts-ignore
-    this.apiService.getAll().subscribe(users=>{
+    this.apiService.getAll().subscribe(users => {
       this.users = users;
+      this.usedTechnologiesList();
+      this.usedLanguagesList();
     });
   }
 
   // @ts-ignore
   usedTechnologiesList(): Technology[] {
-    for(let user of this.users){
+    for (let user of this.users) {
+
       // @ts-ignore
-      for(let tech of user.technologies){
+      for (let tech of user.technologies) {
         // @ts-ignore
-        if(!this.usedTechnologies.includes(tech)){
+        if (!this.usedTechnologies.includes(tech)) {
           // @ts-ignore
           this.usedTechnologies.push(tech);
         }
       }
     }
+  }
+
+  // @ts-ignore
+  usedLanguagesList(): Language[] {
+    for (let user of this.users) {
+      console.log(user)
+      // @ts-ignore
+      for (let lang of user.languages) {
+        if (!this.usedLanguages.includes(lang)) {
+          this.usedLanguages.push(lang);
+        }
+      }
+    }
+
   }
 
 
@@ -63,4 +89,16 @@ export class PeoplePageComponent implements OnInit {
   }
 
 
+  addToLangList(event: any, tech: string) {
+    if (event.target.checked) {
+      if (!this.selectedLanguages.includes(tech)) {
+        this.selectedLanguages.push(tech);
+      }
+    } else {
+      const index = this.selectedLanguages.indexOf(tech);
+      if (index > -1) {
+        this.selectedLanguages.splice(index, 1);
+      }
+    }
+  }
 }
