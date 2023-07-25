83b9509947ebe271494a1b038c6dc455b1c0db77
maxnesss <maxnesazal1492@gmail.com>
2023-07-25 18:34:06 +0200

    Update cart.component.ts

--- frontend/src/app/components/cart/cart.component.ts
+++ frontend/src/app/components/cart/cart.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CartService} from "../../_services/cart.service";
+import {User} from "../../models/user";
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent {
+export class CartComponent implements OnInit{
+  private cartItems:User[] = [];
 
+
+  constructor(private cartService:CartService) {
+
+  }
+  ngOnInit(): void {
+    this.cartItems = this.cartService.getCartItems();
+
+  }
+
+  show() {
+    this.cartItems = this.cartService.getCartItems();
+
+    console.log(this.cartItems);
+  }
 }
