66008fd01ff125dfc59aba490e744c1282a053e8
MaxSlouka <jmslouka@gmail.com>
2023-07-25 13:02:48 +0200

    add new annotation, fix one to many / many to one

--- backend/src/main/java/com/gfa/foxbook/foxbook/models/nonusermodels/Post.java
+++ backend/src/main/java/com/gfa/foxbook/foxbook/models/nonusermodels/Post.java
@@ -1,5 +1,6 @@
 package com.gfa.foxbook.foxbook.models.nonusermodels;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -25,17 +27,15 @@ public class Post {
     private String content;
     private LocalDateTime createdAt;
     private Integer likesCount = 0;
-    private Integer commentsCount = 0;
-
-    public Integer getLikesCount() {
-        return likesCount;
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonManagedReference
+    private List<Comment> comments = new ArrayList<>();
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<Like> likes = new ArrayList<>();
+    public void addComment(Comment comment) {
+        comments.add(comment);
+        comment.setPost(this);
     }
-
-    public void setLikesCount(Integer likesCount) {
-        this.likesCount = likesCount;
-    }
-
-    @OneToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
-    private List<Comment> comments;
 
 }
