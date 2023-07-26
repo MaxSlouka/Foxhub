2c3be8535efd6fbe6509d8fe07a56221901b56ab
TomasMacoun <macount@seznam.cz>
2023-07-26 18:57:57 +0200

    Elements reveal on scroll and hrefs to profiles

--- frontend/src/app/components/creators/creators.component.ts
+++ frontend/src/app/components/creators/creators.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-creators',
   templateUrl: './creators.component.html',
   styleUrls: ['./creators.component.css']
 })
-export class CreatorsComponent {
+export class CreatorsComponent implements OnInit {
 
+  constructor(
+    private route: ActivatedRoute, 
+    private router: Router, 
+    private viewportScroller: ViewportScroller, 
+    private elementRef: ElementRef) {}
+
+  ngOnInit(): void {
+    this.route.fragment.subscribe(fragment => {
+      if (fragment) {
+        this.scrollToElement(fragment);
+      }
+    });
+  }
+
+  scrollToElement(targetID: string) {
+    const targetElement = this.elementRef.nativeElement.querySelector(`#${targetID}`);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  handleClick(event: Event, targetID: string) {
+    event.preventDefault();
+    this.scrollToElement(targetID);
+    this.router.navigate([], { relativeTo: this.route, queryParamsHandling: 'merge' });
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const elementsToReveal = this.elementRef.nativeElement.querySelectorAll('.reveal-section');
+    elementsToReveal.forEach((element: HTMLElement) => {
+      if (!element.classList.contains('reveal')) {
+        if (this.isElementInViewport(element)) {
+          element.classList.add('reveal');
+        }
+      }
+    });
+  }
+
+  isElementInViewport(element: HTMLElement): boolean {
+    const rect = element.getBoundingClientRect();
+    return rect.top >= 0 && rect.bottom <= window.innerHeight;
+  }
+  
 }
