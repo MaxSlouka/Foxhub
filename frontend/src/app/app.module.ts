ea0dc772f46cc19be1b4d374c1ea41fc37616d3d
Deen037 <deen.bds@gmail.com>
2023-07-26 15:43:50 +0200

    href to routerLink

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -42,6 +42,7 @@ import { CartComponent } from './components/cart/cart.component';
 import { CardComponent } from './components/cart/card/card.component';
 import { CartCheckoutComponent } from './components/cart-checkout/cart-checkout.component';
 import { PersonalityComponent } from './components/personality/personality.component';
+import { ItemsCounterComponent } from './components/items-counter/items-counter.component';
 
 
 @NgModule({
@@ -78,7 +79,8 @@ import { PersonalityComponent } from './components/personality/personality.compo
     CartComponent,
     CardComponent,
     CartCheckoutComponent,
-    PersonalityComponent
+    PersonalityComponent,
+    ItemsCounterComponent
 
   ],
   imports: [
