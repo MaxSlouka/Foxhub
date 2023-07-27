e0a011abb8d90579d80294b9a548a5aee9444e4e
TomasMacoun <macount@seznam.cz>
2023-07-27 13:57:09 +0200

    Update personality.service.ts

--- frontend/src/app/_services/personality.service.ts
+++ frontend/src/app/_services/personality.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Personality} from "../models/personality";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Personality } from "../models/personality";
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class PersonalityService {
   readonly urlPersonalities: string = "http://localhost:8080/api/v1/public/personalities";
   constructor(private http: HttpClient) { }
