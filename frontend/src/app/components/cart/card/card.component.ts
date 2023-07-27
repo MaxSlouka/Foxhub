25e84d5bc53ce4e8652c197035b5117871c9608b
TomasMacoun <macount@seznam.cz>
2023-07-27 14:07:54 +0200

    Update card.component.ts

--- frontend/src/app/components/cart/card/card.component.ts
+++ frontend/src/app/components/cart/card/card.component.ts
@@ -1,21 +1,20 @@
-import {Component, Input} from '@angular/core';
-import {User} from "../../../models/user";
-import {CartService} from "../../../_services/cart.service";
+import { Component, Input } from '@angular/core';
+import { User } from "../../../models/user";
+import { CartService } from "../../../_services/cart.service";
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
+
 export class CardComponent {
 
   // @ts-ignore
   @Input() user: User;
-  constructor(private cartService:CartService) {}
+  constructor(private cartService: CartService) { }
 
   removeItem(user: User) {
     this.cartService.removeFromCart(user)
-
-
   }
 }
