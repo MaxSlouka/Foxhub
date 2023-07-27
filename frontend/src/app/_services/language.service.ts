c794956eecdc8ac3565dca21461c6ae889dd4086
TomasMacoun <macount@seznam.cz>
2023-07-27 13:57:07 +0200

    Update language.service.ts

--- frontend/src/app/_services/language.service.ts
+++ frontend/src/app/_services/language.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import {Observable} from "rxjs";
-import {Language} from "../models/language";
-import {HttpClient} from "@angular/common/http";
-
+import { Observable } from "rxjs";
+import { Language } from "../models/language";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class LanguageService {
 
   readonly urlLanguage: string = "http://localhost:8080/api/v1/public/languages";
