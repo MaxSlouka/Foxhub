277b924c74de69d206f19be815c3b730cf99f5ad
Deen037 <deen.bds@gmail.com>
2023-07-27 10:26:52 +0200

    people w prefix

--- frontend/src/app/_services/people.service.ts
+++ frontend/src/app/_services/people.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { User } from "../models/user";
+import { GlobalConstants } from "../common/global-constants";
+
+const prefix = GlobalConstants.prefix;
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +12,7 @@ import { User } from "../models/user";
 
 export class PeopleService {
 
-  private apiUrl: string = "http://localhost:8080/api/v1/public/people";
+  private apiUrl: string = prefix + "/api/v1/public/people";
 
   constructor(private http: HttpClient) {
   }
