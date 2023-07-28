1705a569ca5ce21c0346c8f3256ca62f19ac1736
maxnesss <maxnesazal1492@gmail.com>
2023-07-28 14:40:26 +0200

    pic upload fix test

--- backend/src/main/java/com/gfa/foxbook/foxbook/config/MvcConfig.java
+++ backend/src/main/java/com/gfa/foxbook/foxbook/config/MvcConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-    private String uploadDir = "./uploads";
+    private String uploadDir = "/home/user/uploads";
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
