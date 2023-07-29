7aa5676d553220471d9b618dfa18f382fdc843ec
maxnesss <maxnesazal1492@gmail.com>
2023-07-29 17:33:04 +0200

    update scripts

--- backend/shell_scripts/update.sh
+++ backend/shell_scripts/update.sh
@@ -8,16 +8,25 @@ echo ***************************************************************************
 echo
 #access server
 ssh -p 10922 user@gfapp.eu '
+# git pull
+
 cd Foxbook;
 git pull;
 cd frontend;
+
+# removes old angular build
 rm -rf ../backend/src/main/resources/static;
+
+# makes new angular build
 ng build;
 cd ../backend;
+
+# builds spring
 sh gradlew;
 sh gradlew build;
 '
-# systemctl restart Foxbook
+
+# as a sudo you still have to run: sudo systemctl restart Foxbook
 # exit
 
 
