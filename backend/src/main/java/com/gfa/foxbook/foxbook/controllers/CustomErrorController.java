1ffef6fb573d972ef513c4ec0b0100c0c26c2456
maxnesss <maxnesazal1492@gmail.com>
2023-07-29 22:16:22 +0200

    bug fix

--- backend/src/main/java/com/gfa/foxbook/foxbook/controllers/CustomErrorController.java
+++ backend/src/main/java/com/gfa/foxbook/foxbook/controllers/CustomErrorController.java
@@ -1,14 +1,14 @@
 package com.gfa.foxbook.foxbook.controllers;
-
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class RoutingController {
+public class CustomErrorController implements ErrorController {
 
-    @RequestMapping(value = "{_:^(?!index\\.html|api|\\.).*$}")
-    public String redirectApi() {
+    @RequestMapping("/error")
+    public String handleError() {
         return "forward:/";
     }
-}
 
+}
