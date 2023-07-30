ed54dace66c3250ef09191723f4109e5c8f5a263
maxnesss <maxnesazal1492@gmail.com>
2023-07-30 18:41:20 +0200

    Update admin-board.component.ts

--- frontend/src/app/components/admin-board/admin-board.component.ts
+++ frontend/src/app/components/admin-board/admin-board.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from "../../models/user";
+import {ApiService} from "../../_services/api/api.service";
 
 @Component({
   selector: 'app-admin-board',
   templateUrl: './admin-board.component.html',
   styleUrls: ['./admin-board.component.css']
 })
-export class AdminBoardComponent {
+export class AdminBoardComponent implements OnInit{
+  // @ts-ignore
+  users: User[] = [];
+  // @ts-ignore
+  selectedUser: User;
+
+  constructor(private apiService:ApiService) {
+  }
+
+  ngOnInit(): void {
+    this.apiService.getAll().subscribe(users => {
+      this.users=users;
+      this.selectedUser = users[0];
+    });
+  }
+
+
+
+  deleteUser(user: User) {
+    console.log(user.firstName)
+  }
+
+  setSelectedUser(user: User) {
+    this.selectedUser = user;
+  }
 
 }
