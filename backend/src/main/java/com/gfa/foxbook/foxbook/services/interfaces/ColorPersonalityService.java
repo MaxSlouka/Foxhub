94a77f6f17e002be799e07f6af17774d299e1c43
TomasMacoun <macount@seznam.cz>
2023-08-03 15:11:25 +0200

    Update ColorPersonalityService.java

--- backend/src/main/java/com/gfa/foxbook/foxbook/services/interfaces/ColorPersonalityService.java
+++ backend/src/main/java/com/gfa/foxbook/foxbook/services/interfaces/ColorPersonalityService.java
@@ -1,9 +1,9 @@
 package com.gfa.foxbook.foxbook.services.interfaces;
 
-import com.gfa.foxbook.foxbook.models.nonusermodels.Personality;
+import com.gfa.foxbook.foxbook.models.nonusermodels.ColorPersonality;
 
 import java.util.List;
 
 public interface ColorPersonalityService {
-    List<Personality> getAllColorPersonalities();
+    List<ColorPersonality> getAllColorPersonalities();
 }
