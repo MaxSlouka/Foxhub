33c23448f682682646e69e90a21adf34f2ec5613
TomasMacoun <macount@seznam.cz>
2023-08-03 14:02:24 +0200

    Create ColorPersonalityService.java

---
+++ backend/src/main/java/com/gfa/foxbook/foxbook/services/interfaces/ColorPersonalityService.java
@@ -0,0 +1,9 @@
+package com.gfa.foxbook.foxbook.services.interfaces;
+
+import com.gfa.foxbook.foxbook.models.nonusermodels.Personality;
+
+import java.util.List;
+
+public interface ColorPersonalityService {
+    List<Personality> getAllColorPersonalities();
+}
