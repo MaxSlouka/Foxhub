015e2d747a08ac4de11069b69c188a2245abea36
TomasMacoun <macount@seznam.cz>
2023-08-07 16:35:11 +0200

    Update api.service.ts

--- frontend/src/app/_services/api/api.service.ts
+++ frontend/src/app/_services/api/api.service.ts
@@ -1,21 +1,23 @@
-import {Injectable} from '@angular/core';
-import {Observable} from "rxjs";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {User} from "../../models/user";
-import {Language} from "../../models/language";
-import {Technology} from "../../models/technology";
-import {Location} from "../../models/location";
-import {Personality} from "../../models/personality";
-import {GlobalConstants} from "../../common/global-constants";
-import {Router} from "@angular/router";
-import {catchError} from 'rxjs/operators';
-import {of} from 'rxjs';
-import {ColorPersonality} from 'src/app/models/colorPersonality';
+import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { User } from "../../models/user";
+import { Language } from "../../models/language";
+import { Technology } from "../../models/technology";
+import { Location } from "../../models/location";
+import { Personality } from "../../models/personality";
+import { GlobalConstants } from "../../common/global-constants";
+import { Router } from "@angular/router";
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { ColorPersonality } from 'src/app/models/colorPersonality';
+import { DonutFilling } from 'src/app/models/donutFilling';
+import { SpiritAnimal } from 'src/app/models/spiritAnimal';
 
 const prefix = GlobalConstants.prefix;
 
 const httpOptions = {
-  headers: new HttpHeaders({'Content-type': 'application/json'})
+  headers: new HttpHeaders({ 'Content-type': 'application/json' })
 }
 
 @Injectable({
@@ -75,43 +77,47 @@ export class ApiService {
   }
 
   updateUser(firstName: string,
-             lastName: string,
-             phone: string | undefined,
-             locations: Location[] | undefined,
-             oneLineAbout: string | undefined,
-             workPreference: string | undefined,
-             about: string | undefined,
-             gitHub: string | undefined,
-             linkedin: string | undefined,
-             facebook: string | undefined,
-             instagram: string | undefined,
-             optionalPage: string | undefined,
-             languages: Language[] | undefined,
-             technologies: Technology[] | undefined,
-             personality: Personality | undefined,
-             colorPersonality: ColorPersonality | undefined,
-             yearOfBirth: number | undefined
+    lastName: string,
+    phone: string | undefined,
+    locations: Location[] | undefined,
+    oneLineAbout: string | undefined,
+    workPreference: string | undefined,
+    about: string | undefined,
+    gitHub: string | undefined,
+    linkedin: string | undefined,
+    facebook: string | undefined,
+    instagram: string | undefined,
+    optionalPage: string | undefined,
+    languages: Language[] | undefined,
+    technologies: Technology[] | undefined,
+    personality: Personality | undefined,
+    colorPersonality: ColorPersonality | undefined,
+    yearOfBirth: number | undefined,
+    donutFilling: DonutFilling | undefined,
+    spiritAnimal: SpiritAnimal | undefined
   ): Observable<any> {
 
     return this.http.patch(prefix + "/api/v1/user/people", {
-        firstName,
-        lastName,
-        phone,
-        locations,
-        oneLineAbout,
-        workPreference,
-        about,
-        gitHub,
-        linkedin,
-        facebook,
-        instagram,
-        optionalPage,
-        languages,
-        technologies,
-        personality,
-        colorPersonality,
-        yearOfBirth
-      },
+      firstName,
+      lastName,
+      phone,
+      locations,
+      oneLineAbout,
+      workPreference,
+      about,
+      gitHub,
+      linkedin,
+      facebook,
+      instagram,
+      optionalPage,
+      languages,
+      technologies,
+      personality,
+      colorPersonality,
+      yearOfBirth,
+      donutFilling,
+      spiritAnimal
+    },
       httpOptions
     );
   }
